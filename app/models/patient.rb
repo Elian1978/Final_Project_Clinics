6a657e9e584b597ccfcb3e9016ec81b01cb5ded5
Elian Rojas <impresosgosen@gmail.com>
2021-04-22 20:25:51 -0600

    Add devise and Patients

---
+++ app/models/patient.rb
@@ -0,0 +1,7 @@
+class Patient < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  attr_accesible :email, :encrypted_password, :first_name, :last_name, :age, :language, :sex, :phone, :provincia, :canton, :distrito, :barrio  
+end
