b321f5c7ef6e654b6660ec7828cf7a20997a523e
Elian Rojas <impresosgosen@gmail.com>
2021-04-24 12:38:47 -0600

    Add footer

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,5 +3,4 @@ class Patient < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  attr_accessor :email, :encrypted_password, :first_name, :last_name, :age, :language, :sex, :phone, :provincia, :canton, :distrito, :barrio  
 end
