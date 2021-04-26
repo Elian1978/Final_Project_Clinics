65cc479b9a70b033965774af84edb055ca3fd40e
Elian Rojas <impresosgosen@gmail.com>
2021-04-26 11:31:52 -0600

    Add seed

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,6 +2,7 @@ class Doctor < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :notes, dependent: :destroy
+  has_many :patients, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Clinic.destroy_all 
+
+10.times do 
+    clinic = Clinic.create(
+      name: Faker::Name.name,
+      phone: Faker::PhoneNumber.cell_phone,
+      email: Faker::Internet.email,
+      adress: Faker::Address.city,
+      information: Faker::Lorem.sentence(word_count: 5),
+      logo: " "
+     
+    )
+end
+
+10.times do 
+      doctor = Doctor.create(
+        email: Faker::Internet.email,
+        encrypted_password: '123123',
+        name: Faker::Name.name,
+        specialty: Faker::Educator.subject,
+        school: Faker::Educator.university,
+        phone: Faker::PhoneNumber.cell_phone,
+       
+      )
+end
+
+10.times do
+    patient = Patient.create(
+        email: Faker::Internet.email,
+        encrypted_password: '123123',
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        age: Faker::Number.between(from: 1, to: 120),
+        language: "Ingles",
+        sex: Faker::Demographic.sex,
+        phone:  Faker::PhoneNumber.cell_phone,
+        provincia: Faker::Address.city,
+        canton: Faker::Address.state,
+        distrito: "Guapiles",
+        barrio: Faker::Address.street_address,
+
+    )
+end 
+   
+
+
+
+   
