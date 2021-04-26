0283f818cc7af20189ad28e8b5d7b142ea588d7d
Elian Rojas <impresosgosen@gmail.com>
2021-04-26 10:19:28 -0600

    Add relations notes, patients, doctors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_23_200115) do
+ActiveRecord::Schema.define(version: 2021_04_26_161128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,17 @@ ActiveRecord::Schema.define(version: 2021_04_23_200115) do
     t.index ["reset_password_token"], name: "index_doctors_on_reset_password_token", unique: true
   end
 
+  create_table "notes", force: :cascade do |t|
+    t.string "message"
+    t.string "consult_reason"
+    t.bigint "patient_id", null: false
+    t.bigint "doctor_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["doctor_id"], name: "index_notes_on_doctor_id"
+    t.index ["patient_id"], name: "index_notes_on_patient_id"
+  end
+
   create_table "patients", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -64,4 +75,6 @@ ActiveRecord::Schema.define(version: 2021_04_23_200115) do
     t.index ["reset_password_token"], name: "index_patients_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "notes", "doctors"
+  add_foreign_key "notes", "patients"
 end
