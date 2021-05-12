219cabe6869d7354d2c84ddbce74981bf54d5198
Elian Rojas <impresosgosen@gmail.com>
2021-05-12 02:07:25 -0600

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,35 +13,35 @@
 #     )
 # end
 
-# 5.times do 
-#       doctor = Doctor.create(
-#         email: Faker::Internet.email,
-#         password: '123123',
-#         name: Faker::Name.name,
-#         specialty: Faker::Educator.subject,
-#         school: Faker::Educator.university,
-#         phone: Faker::PhoneNumber.cell_phone,
+5.times do 
+      doctor = Doctor.create(
+        email: Faker::Internet.email,
+        password: '123123',
+        name: Faker::Name.name,
+        specialty: Faker::Educator.subject,
+        school: Faker::Educator.university,
+        phone: Faker::PhoneNumber.cell_phone,
        
-#       )
-# end
+      )
+end
 
-# 5.times do
-#     patient = Patient.create(
-#         email: Faker::Internet.email,
-#         password: '123123',
-#         first_name: Faker::Name.first_name,
-#         last_name: Faker::Name.last_name,
-#         age: Faker::Number.between(from: 1, to: 120),
-#         language: "Ingles",
-#         sex: Faker::Demographic.sex,
-#         phone:  Faker::PhoneNumber.cell_phone,
-#         provincia: Faker::Address.city,
-#         canton: Faker::Address.state,
-#         distrito: "Guapiles",
-#         barrio: Faker::Address.street_address,
+5.times do
+    patient = Patient.create(
+        email: Faker::Internet.email,
+        password: '123123',
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        age: Faker::Number.between(from: 1, to: 120),
+        language: "Ingles",
+        sex: Faker::Demographic.sex,
+        phone:  Faker::PhoneNumber.cell_phone,
+        provincia: Faker::Address.city,
+        canton: Faker::Address.state,
+        distrito: "Guapiles",
+        barrio: Faker::Address.street_address,
 
-#     )
-# end 
+    )
+end 
 
 Admin.create!(email: 'admin@example.com', password: '123123') if Rails.env.development?
    
